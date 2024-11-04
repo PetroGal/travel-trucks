66b2da2145d7ea3ff5691c13e06b0420bff65eda
PetroGal <69636047+PetroGal@users.noreply.github.com>
2024-11-04 18:58:48 +0200

    sets up project and folder structure, configures redux store

---
+++ src/redux/store.js
@@ -0,0 +1,9 @@
+import { configureStore } from "@reduxjs/toolkit"
+
+export const store = configureStore({
+  reducer: {
+    // We'll add our reducers here as we create slices
+  },
+})
+
+export default store
